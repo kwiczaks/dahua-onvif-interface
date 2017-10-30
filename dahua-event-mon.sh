5d2c6728d5ace2ad0d3fd8992b4da98964d901a1
kwiczaks <kwiczaks@tlen.pl>
2017-10-30 13:22:43 +0100

    Create dahua-event-mon.sh

---
+++ dahua-event-mon.sh
@@ -0,0 +1,30 @@
+##
+PIDFILE=/tmp/${0##*/}.pid
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Job is already running"
+    exit 1
+  else
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
+read -n1 -r -p "Press any key to close proces and free PID file..." key
+
+rm $PIDFILE
